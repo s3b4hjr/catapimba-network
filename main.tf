6c9a86fd8204ff17e867cbd67c060c6fd78a472d
Sebastiao Junior <sebastiao.junior@tc.com.br>
2022-08-18 22:28:17 -0300

    CC Tag

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "catapimba-corps-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-east-1a", "eu-east-1b"]
+  azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
@@ -15,5 +15,7 @@ module "vpc" {
   tags = {
     Terraform   = "true"
     Environment = "dev"
+    Name = "catapimba-corps-vpc"
+    CC = "12345"
   }
 }
